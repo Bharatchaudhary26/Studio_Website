8bef41018a39935a8481015a6eed41338b56eeb3
Bharat Chaudhary <83763139+Bharatchaudhary26@users.noreply.github.com>
2023-10-29 14:38:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  //header('Location: https://'.$_SERVER['HTTP_HOST'].'/administrator/index.php');
+  
+  //header('Location: http://'.$_SERVER['HTTP_HOST'].'/harienterprise/administrator/index.php');
+  
+  //header('Location: http://'.$_SERVER['HTTP_HOST'].'/nnaadmin/administrator/index.php');
+  
+  //header('Location: http://'.$_SERVER['HTTP_HOST'].'/nnaadmin/admission/index.php');
+   
+   header('Location: Website/index.php');
+?>
